78024ff5b714dd7e25242e622b3dd6a20baabd94
Barbu Adrian <barbucadrian@gmail.com>
2025-03-16 13:35:43 +0200

    Lab 02:     - Exercise #pixels:
    Signed-off-by: Barbu Adrian <barbucadrian@gmail.com>

--- labs/lab-02/tasks/find_max/support/find_max.c
+++ labs/lab-02/tasks/find_max/support/find_max.c
@@ -11,25 +11,17 @@ void *find_max(void *arr, int n, size_t element_size,
 {
 	void *max_elem = arr;
 
-	(void) n;
-	(void) element_size;
-	(void) compare;
-
-	/**
-	 * TODO: Implement function
-	 */
+	for (int i = 1; i < n; i++)
+		if (compare((char*)arr + i * element_size, max_elem) > 0)
+			max_elem = (char*)arr + i * element_size;
 
 	return max_elem;
 }
 
 int compare(const void *a, const void *b)
 {
-	(void) a;
-	(void) b;
+	int *nra = (int*)a;
+	int *nrb = (int*)b;
 
-	/**
-	 * TODO: Implement function
-	 */
-
-	return 0x69;
+	return *nra - *nrb;
 }
--- labs/lab-02/tasks/pixels/support/pixels.c
+++ labs/lab-02/tasks/pixels/support/pixels.c
@@ -11,10 +11,21 @@
 
 void reverse_pic(struct picture *pic)
 {
-	(void) pic;
+	for (int i = 0; i < pic->height / 2; i++) {
+		for (int j = 0; j < pic->width; j++) {
+			struct pixel aux = GET_PIXEL(pic->pix_array, i, j);
+			GET_PIXEL(pic->pix_array, i, j) = GET_PIXEL(pic->pix_array, pic->height - i - 1, j);
+			GET_PIXEL(pic->pix_array, pic->height - i - 1, j) = aux;
+		}
+	}
 }
 
 void color_to_gray(struct picture *pic)
 {
-	(void) pic;
+	for (int i = 0; i < pic->height; i++)
+		for (int j = 0; j < pic->width; j++) {
+			GET_PIXEL(pic->pix_array, i, j).R *= 0.3;
+			GET_PIXEL(pic->pix_array, i, j).G *= 0.59;
+			GET_PIXEL(pic->pix_array, i, j).B *= 0.11;
+		}
 }
