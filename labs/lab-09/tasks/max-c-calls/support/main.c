b820089bd2c7b6388b355af00468b138ec78841b
Radu Mantu <andru.mantu@gmail.com>
2025-05-11 19:12:50 +0300

    labs/lab-09: Changed max-c-calls task
    Fixes #85

--- labs/lab-09/tasks/max-c-calls/support/main.c
+++ labs/lab-09/tasks/max-c-calls/support/main.c
@@ -2,16 +2,17 @@
 
 #include <stdio.h>
 
-int get_max(unsigned int *arr, unsigned int size);
+unsigned int get_max(unsigned int *arr, unsigned int len);
 
 int main(void)
 {
-	unsigned int arr[] = { 19, 7, 129, 87, 54, 218, 67, 12, 19, 99 };
-	unsigned int max;
+    unsigned int arr[] = { 19, 7, 129, 87, 54, 218, 67, 12, 19, 99 };
+    unsigned int max;
 
-	max = get_max(arr, sizeof(arr) / sizeof(arr[0]));
+    max = get_max(arr, 10);
 
-	printf("max: %u\n", max);
+    printf("max: %u\n", max);
 
-	return 0;
+    return 0;
 }
+
