dcedb9d6c220c61e615c3bcc741505c52a0494a5
teodutu <teodutu@users.noreply.github.com>
2025-06-09 09:32:54 +0000

    deploy: 85dc84fc7a67d798e303ed110755feaa1da5e4a7

---
+++ x86_64/labs/lab-08/tasks/rot13/support/rot13.asm
@@ -0,0 +1,15 @@
+section .text
+global rot13
+
+rot13:
+    push rbp
+    mov rbp, rsp
+
+    ; TODO: save the used registers and align the stack, if needed
+
+    ; TODO
+
+    ; TODO: restore the used registers and the stack pointer, if altered
+
+    leave
+    ret
