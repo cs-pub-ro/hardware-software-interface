9b1324b66023e3c9baaa976f3ebd2a77c887ce41
Stefan Alexandru <alexandru.stefan06@stud.acs.upb.ro>
2026-03-20 14:04:23 +0200

    Lab03 : binary_search.c : Add solution
    Binary search implemented only using goto and if statements
    
    Signed-off-by: Stefan Alexandru <alexandru.stefan06@stud.acs.upb.ro>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -9,9 +9,22 @@ int binary_search(int *v, int len, int dest)
 	int start = 0;
 	int end = len - 1;
 	int middle;
-
-	/**
-	 * TODO: Implement binary search
-	 */
+label:
+	if (start > end) {
+		goto nasol;
+	}
+	if (start <= end) {
+		middle = (start + end) / 2;
+	}
+	if (dest < *(v + middle)) {
+			end = middle - 1;
+			goto label;
+	}
+	if (dest > *(v + middle)) {
+		start = middle + 1;
+		goto label;
+	}
+	if (dest == *(v + middle)) return middle;
+nasol:
 	return -1;
 }
