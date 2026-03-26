0cbdcc6548fe5fbf9da4743d557c80b8fee17b9d
Apostol Alexandru-Gabriel <alexandru.apostol06@stud.acs.upb.ro>
2026-03-26 09:59:47 +0200

    Add solution
    It works, but I only get 50/100 because it gets stuck at the 3rd test.
    
    Signed-off-by: Apostol Alexandru-Gabriel <alexandru.apostol06@stud.acs.upb.ro>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -9,9 +9,24 @@ int binary_search(int *v, int len, int dest)
 	int start = 0;
 	int end = len - 1;
 	int middle;
-
-	/**
-	 * TODO: Implement binary search
-	 */
+START:
+	if(end < start) {
+		goto NOT_FOUND;
+	}
+	middle = start + (end - start) / 2;
+	if (v[middle] == dest) {
+		goto FOUND;
+	}
+	if (v[middle] < dest) {
+		goto BIGGER;
+	}
+	end = middle + 1;
+	goto START;
+BIGGER:
+	start = middle + 1;
+	goto START;
+FOUND:
+	return middle;
+NOT_FOUND:
 	return -1;
 }
